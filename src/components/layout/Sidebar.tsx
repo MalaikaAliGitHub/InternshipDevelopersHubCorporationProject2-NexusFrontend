ecf955f6852bc1efe3cd3eec832a4e927d246185
MalaikaAliGitHub <malaikaa7474@gmail.com>
2025-09-01 03:19:51 -0700

    Added Documents link to investor sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -54,6 +54,7 @@ export const Sidebar: React.FC = () => {
     { to: '/entrepreneurs', icon: <Users size={20} />, text: 'Find Startups' },
     { to: '/messages', icon: <MessageCircle size={20} />, text: 'Messages' },
     { to: '/notifications', icon: <Bell size={20} />, text: 'Notifications' },
+    { to: '/documents', icon: <FileText size={20} />, text: 'Documents' },
     { to: '/deals', icon: <FileText size={20} />, text: 'Deals' },
     { to: '/payment', icon: <CreditCard size={20} />, text: 'Payment' },
   ];
